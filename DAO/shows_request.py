6bfa998c1b71027734b4b1472690f08e89db7cdb
Vinícius souza <vinicanal604@gmail.com>
2024-11-17 12:05:09 -0300

    Crescent v2.0

--- DAO/shows_request.py
+++ DAO/shows_request.py
@@ -3,7 +3,7 @@ from database.database import Shows
 
 def shows_request():
     page = request.args.get('page', 1, type=int)
-    per_page = 6 
+    per_page = 1
     shows = Shows.query.paginate(page=page, per_page=per_page)
     return shows
 
